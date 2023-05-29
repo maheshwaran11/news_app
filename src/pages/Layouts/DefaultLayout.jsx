3a9499f016172c8b22fe579ec309215b155b1ef6
Thiri@1234 <maheshwaran11@gmail.com>
2023-05-29 14:23:16 +0530

    First commit

--- src/pages/Layouts/DefaultLayout.jsx
+++ src/pages/Layouts/DefaultLayout.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
 import NavBar from '../../components/NavBar';
 import Footer from '../../components/Footer';
-import { Route, Routes } from 'react-router-dom';
 
 const DefaultLayout = ({children}) => { 
 
 
   return (
     <>
-        <NavBar />
-
-        <Routes>
-            <Route path='/' exact element={<Home />} key="0"></Route>
-            <Route path='home' element={<Home />} key="1"></Route>
-            <Route path='cinema' element={<Cinema />}  key="2"></Route>
-            <Route path='contact' element={<About />}  key="3"></Route>
-            <Route path='login' element={<Login />}  key="4"></Route>
-            <Route path='register' element={<Register />}  key="5"></Route>
-            <Route path='dashboard' element={<Dashboard />}  key="6"></Route>
-            <Route path='addCategory' element={<AddCategory />}  key="7"></Route>
-            <Route path='addNews' element={<AddNews />}  key="6"></Route>
-            <Route path='newsDetails/:id/:title' element={<NewsDetails />}  key="6"></Route>
-            <Route path='categories/:id/:title' element={<Categories />}  key="6"></Route>
-          </Routes>
-
+        {/* <NavBar /> */}
         <main>
             {children}
         </main>
